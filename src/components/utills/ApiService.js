9014be34b652b990c748ba1ed22331ad64d16521
PetiurAliona <alena.petyur@gmail.com>
2021-08-28 18:20:03 +0300

    deleted comment

--- src/components/utills/ApiService.js
+++ src/components/utills/ApiService.js
@@ -1,8 +1,6 @@
 const apiKey = "22768423-65df9381e505940710ceeb6f4"
 const perPage = 12
 
-// https://pixabay.com/api/?q=что_искать&page=номер_страницы&key=твой_ключ&image_type=photo&orientation=horizontal&per_page=12
-
 function fetchQuery() {
   const apiUrl = `https://pixabay.com/api/?q=${this.searchQuery}&page=${this.page}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=${perPage}`
   return fetch(apiUrl).then((response) => {
